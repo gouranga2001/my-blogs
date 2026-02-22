3746d6f309491bc1582589ee1bb1da4a3cbcefd7
gouranga2001 <sumangouranga@outlook.com>
2026-02-22 12:45:53 +0530

    updated README and removed unused code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -67,23 +67,5 @@ class UserController extends Controller
         return back()->with('success','profile updated successfully');
     }
 
-    // public function login(Request $request)
-    //     {
-    //         $credentials = $request->validate([
-    //             'email' => 'required|email',
-    //             'password' => 'required'
-    //         ]);
-
-    //         if (Auth::attempt($credentials)) {
-    //             $request->session()->regenerate();
-
-    //             return redirect()->route('admin.dashboard')
-    //                 ->with('success', 'Logged in successfully!');
-    //         }
-
-    //         return back()->withErrors([
-    //             'email' => 'Invalid credentials.'
-    //         ])->onlyInput('email');
-    //     }
 
 }
